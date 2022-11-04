7d769c8c3995affe26644c75c466e8b9cf16f11b
Vedant Asati <vedant.asati03@gmail.com>
2022-11-04 22:46:59 +0530

    added auto copying to clipboard

--- password_generator.py
+++ password_generator.py
@@ -1,43 +1,17 @@
 """
-password_generator
+password generator
 """
 
 import random
 import string
 import sys
+import pyperclip
 
 
-def generates_pin(password_length):
+def main():
     """
-    This function Generates pin
+    user interacts here
     """
-    pin = ""
-    for _ in range(password_length):
-        pin += str(random.randint(0, 9))
-    with open(
-        "Passwords_Manager\\pin_passwords.txt", "a", encoding=("UTF-8")
-    ) as generated_passwords:
-        generated_passwords.write(f"Your generated PIN is: {pin}\n")
-    print(f"Your PIN is: {pin}\n")
-
-
-def generates_password(password_length):
-    """
-    This function Generates password
-    """
-    password_list = [string.digits, string.ascii_letters, string.punctuation]
-    characters = list("".join([element for element in password_list]))
-    password = ""
-    for _ in range(password_length):
-        password += random.choice(characters)
-    with open(
-        "Passwords_Manager\\pin_passwords.txt", "a", encoding=("UTF-8")
-    ) as generated_passwords:
-        generated_passwords.write(f"Your generated password is: {password}\n")
-    print(f"Your password is: {password}\n")
-
-
-if __name__ == "__main__":
     print("Press 1 for PIN\nPress 2 for PASSWORD\n")
     print("Password length must be atleast 8 characters.\n")
     password_type = input("What type of password you want: ")
@@ -55,3 +29,37 @@ if __name__ == "__main__":
 
     else:
         print("Invalid input!")
+
+
+def generates_pin(password_length):
+    """
+    This function Generates pin
+    """
+    pin = ""
+    for _ in range(password_length):
+        pin += str(random.randint(0, 9))
+    with open("pin_passwords.txt", "a", encoding=("UTF-8")) as generated_passwords:
+        generated_passwords.write(f"Your generated PIN is: {pin}\n")
+    pyperclip.copy(pin)
+    print(f"Your PIN is: {pin}\n\nNote: Your PIN is copied to your clipboard\n")
+
+
+def generates_password(password_length):
+    """
+    This function Generates password
+    """
+    password_list = [string.digits, string.ascii_letters, string.punctuation]
+    characters = list("".join([element for element in password_list]))
+    password = ""
+    for _ in range(password_length):
+        password += random.choice(characters)
+    with open("pin_passwords.txt", "a", encoding=("UTF-8")) as generated_passwords:
+        generated_passwords.write(f"Your generated password is: {password}\n")
+    pyperclip.copy(password)
+    print(
+        f"Your password is: {password}\n\nNote: Your password is copied to your clipboard\n"
+    )
+
+
+if __name__ == "__main__":
+    main()
