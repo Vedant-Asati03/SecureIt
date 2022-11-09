08bf1f45c0b64dd6bbbd71bddebd99e6e19ffb59
Vedant Asati <vedant.asati03@gmail.com>
2022-11-10 00:38:32 +0530

    added colors

--- password_generator.py
+++ password_generator.py
@@ -8,18 +8,26 @@ import sys
 import json
 import pyperclip
 import cryptocode
+from rich.text import Text
+from rich.console import Console
 from sign_in import create_account, check_existing_user
+
+
+console = Console()
 
 
 def main():
     """
     user interacts here
     """
-    print("Press 1 for PIN\nPress 2 for PASSWORD\n")
-    print("Password length must be atleast 8 characters.\n")
 
-    password_type = input("What type of password you want: ")
-    password_len = int(input("Length of password: "))
+    console.print("Press 1 for PIN\nPress 2 for PASSWORD\n", style="#F8CB2E")
+    console.print("Password length must be atleast 8 characters.\n", style="#FF1E00")
+
+    password_type = console.input(
+        Text("What type of password you want: ", style="#B4B897")
+    )
+    password_len = int(console.input(Text("Length of password: ", style="#B4B897")))
 
     if password_len < 8:
         print("Read instructions carefully!")
@@ -29,10 +37,7 @@ def main():
         generates_pin(password_len)
 
     elif password_type == "2":
-        print(generates_password(password_len))
-
-    else:
-        print("Invalid input!")
+        generates_password(password_len)
 
 
 def generates_pin(pin_length):
@@ -43,8 +48,13 @@ def generates_pin(pin_length):
     for _ in range(pin_length):
         pin += str(random.randint(0, 9))
     pyperclip.copy(pin)
-    print(f"Your PIN is: {pin}\n\nNote: Your PIN is copied to your clipboard\n")
-    create_account(username, cryptocode.encrypt(pin, "3284528345323"))
+    console.print(
+        f"Your PIN is: [u #DFDFDE]{pin}[/u #DFDFDE]\n\n[u #24A19C]Note: Your PIN is copied to your clipboard[/u #24A19C]\n",
+        style="#B4B897",
+    )
+    match ask_user:
+        case "Y":
+            create_account(username, cryptocode.encrypt(pin, "3284528345323"))
 
 
 def generates_password(password_length):
@@ -57,38 +67,61 @@ def generates_password(password_length):
     for _ in range(password_length):
         password += random.choice(characters)
     pyperclip.copy(password)
-    print(
-        f"Your password is: {password}\n\nNote: Your password is copied to your clipboard\n"
+    console.print(
+        f"Your password is: [u #DFDFDE]{password}[/u #DFDFDE]\n\n[u #24A19C]Note: Your PIN is copied to your clipboard[/u #24A19C]\n",
+        style="#B4B897",
     )
-    create_account(username, cryptocode.encrypt(password, "3284528345323"))
+    match ask_user:
+        case "Y":
+            create_account(username, cryptocode.encrypt(password, "3284528345323"))
 
 
-def view_userdata(command:str):
+def view_userdata(_: str):
     """
     this function lets user to view their saved passwords
     """
-    user_data = input("Enter username: ").removesuffix(".json")
+    user_data = (
+        console.input(Text("Enter username: ", style="#B4B897"))
+        .removesuffix(".json")
+        .capitalize()
+    )
     with open(("users\\" + user_data + ".json"), "r", encoding="UTF-8") as user:
-        fetch_password = json.load(user)
-        decrypted_password = cryptocode.decrypt(fetch_password[user_data], "3284528345323")
-        print(decrypted_password)
+        fetch_password = json.load(user)[user_data]
+        decrypted_password = cryptocode.decrypt(fetch_password, "3284528345323")
+        copy = console.input(
+            Text(
+                f"Your password is: {decrypted_password}\nPress 'c' to copy this password: ",
+                style="#B4B897",
+            )
+        )
+        match copy:
+            case "c":
+                pyperclip.copy(decrypted_password)
+                return "copied"
+            case _:
+                sys.exit()
 
 
 if __name__ == "__main__":
-    ask_user = input("Do you want to login[y/n] or view existing user[v]: ")
+    ask_user = console.input(
+        Text("Do you want to login[y/n] or view existing user[v]: ", style="#B4B897")
+    ).capitalize()
+    match ask_user:
 
-    if ask_user == "y":
-        username = input("Enter your username: ")
-        try:
-            check_existing_user(username)
-        except:
+        case "Y":
+            username = console.input(
+                Text("Enter your username: ", style="#B4B897")
+            ).capitalize()
+            try:
+                check_existing_user(username)
+            except:
+                main()
+
+        case "N":
             main()
 
-    elif ask_user == "n":
-        main()
-
-    elif ask_user == "v":
-        try:
-            view_userdata("v")
-        except:
-            print("User doesn't exits")
+        case "V":
+            try:
+                view_userdata("V")
+            except:
+                console.print("User doesn't exits", style="#24A19C")
