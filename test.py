b409ff0301e4b739041ea3514a7bf3945b08ed61
Vedant Asati <109758134+Vedant-Asati03@users.noreply.github.com>
2022-11-09 23:31:27 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-json_dump = json.dumps({"hi":"bye"}, indent=4)
-
-with open("sample.json", "w", encoding="UTF-8") as outfile:
-    outfile.write(json_dump)
-
-with open("sample.json", "r", encoding="UTF-8") as outfile:
-    print(json.load(outfile["hi"]))
